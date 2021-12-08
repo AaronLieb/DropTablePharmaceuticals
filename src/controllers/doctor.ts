4ebfd1fbcb98297d81668e9cda3bb07206bed239
Peekoe <peekoecode@gmail.com>
2021-12-08 15:00:47 -0800

    add doctor route, small changes

--- src/controllers/doctor.ts
+++ src/controllers/doctor.ts
@@ -22,10 +22,26 @@ const getAll = (req: Request, res: Response) => {
 
 // req : { id: int }
 // res : { result }
-const getPatientDoctor = (req: Request, res: Response) => {
+const getDoctor = (req: Request, res: Response) => {
     logging.info(NAMESPACE, `getPatientDoctor called.`);
 
-    mysql.db.query('SELECT * FROM patient WHERE  ', [req.body['id']],
+    mysql.db.query('SELECT * FROM doctor WHERE Doctor_ID = ?', [req.body['id']],
+     (error, result) => {
+        if (error) {
+            logging.error(NAMESPACE, 'Could not perform query', error);
+            res.status(400).json(error);
+        } else {
+            return res.status(200).json({
+                result: result
+            });
+        }
+    });
+};
+
+const getDoctorPatients = (req: Request, res: Response) => {
+    logging.info(NAMESPACE, `getPatientDoctor called.`);
+
+    mysql.db.query('SELECT * FROM patient WHERE primaryDoctor = ?', [req.body['id']],
      (error, result) => {
         if (error) {
             logging.error(NAMESPACE, 'Could not perform query', error);
@@ -57,6 +73,7 @@ const postDoctor = (req: Request, res: Response) => {
 
 export default {
     getAll,
-    getPatientDoctor,
+    getDoctor,
+    getDoctorPatients,
     postDoctor
 };
